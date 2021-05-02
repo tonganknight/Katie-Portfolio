5ecaf0dd5fb371f84b0091d2882a30d6018e6695
Malachi <malachialusa@gmail.com>
2021-05-02 15:53:39 -0600

    I added icons and links, and effects to nav section

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -21,7 +21,12 @@ function Nav({currentPage, handlepageChange}) {
                         } >{tab}</a></div>
                     ))}
 
-            
+                <div className="iconflex">
+                    <a  target="blank" href="https://www.facebook.com/katelynn.empey" className="iconFacebook"></a>
+                    <a  target="blank" href="https://www.instagram.com/alusadesign" className="iconInstagram"></a>
+                    <a  target="blank" href="https://www.linkedin.com/in/katelynn-empey-76b6ab4b/" className="iconLink"></a>
+                    <a  target="blank" href="https://www.behance.net/KatieAlusa?tracking_source=search_users_recommended%7CKatie%20alusa" className="iconBehance"></a>
+                </div>
             </nav>
 
  
